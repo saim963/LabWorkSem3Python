24b534b0c79c4071c9097c6887fca034724e5583
Saim Shakeel <aims3328@gmail.com>
2024-10-03 00:40:35 +0530

    minor

---
+++ week6_q3.py
@@ -0,0 +1,13 @@
+#  Enter a 4 digit number and sum sq of first and last two
+def foo():
+    num = input('Enter a 4 digit number: ')
+    if len(num) !=4 :
+        print('Try Again:')
+        foo()
+        
+    start = int(num[:2])
+    end = int(num[2:])
+    result = start*start + end*end
+    print(f'Result squared sum: {result}')
+
+foo()
